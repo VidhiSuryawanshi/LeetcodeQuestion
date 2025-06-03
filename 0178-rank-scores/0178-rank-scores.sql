d1b3c964f72912220a967da8a735feec8148ede5
VidhiSuryawanshi <suryawanshividhi6@gmail.com>
2025-06-03 22:05:17 +0530

    Time: 775 ms (9.28%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,10 +1,15 @@
--- SELECT score ,  AS rank
--- FROM Scores
--- GROUP BY score
--- ORDER BY score DESC
-SELECT s1.Score,
-       (SELECT COUNT(DISTINCT s2.Score) 
-        FROM Scores s2 
-        WHERE s2.Score > s1.Score) + 1 AS 'Rank'
-FROM Scores s1
-ORDER BY s1.Score DESC;
+SELECT S1.score ,  
+    (SELECT COUNT(DISTINCT s2.score)
+    FROM Scores s2
+    WHERE s2.score > S1.score )+1 AS 'rank'
+FROM Scores S1
+ORDER BY S1.score DESC
+
+
+
+-- SELECT s1.Score,
+--        (SELECT COUNT(DISTINCT s2.Score) 
+--         FROM Scores s2 
+--         WHERE s2.Score > s1.Score) + 1 AS 'Rank'
+-- FROM Scores s1
+-- ORDER BY s1.Score DESC;

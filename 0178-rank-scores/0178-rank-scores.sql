fb69dd2585972cb370975249f5d951202bbe0862
VidhiSuryawanshi <suryawanshividhi6@gmail.com>
2025-06-03 22:00:28 +0530

    Time: 934 ms (5.02%), Space: 0B (100%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,10 @@
+-- SELECT score ,  AS rank
+-- FROM Scores
+-- GROUP BY score
+-- ORDER BY score DESC
+SELECT s1.Score,
+       (SELECT COUNT(DISTINCT s2.Score) 
+        FROM Scores s2 
+        WHERE s2.Score > s1.Score) + 1 AS 'Rank'
+FROM Scores s1
+ORDER BY s1.Score DESC;

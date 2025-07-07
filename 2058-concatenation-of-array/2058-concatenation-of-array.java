457ae4f22a332b9c5dfe67e43e53c5aaf92f6066
VidhiSuryawanshi <suryawanshividhi6@gmail.com>
2025-07-07 17:00:40 +0530

    Time: 1 ms (96.45%), Space: 45.2 MB (82.86%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -5,9 +5,7 @@ class Solution {
 
         for(int i=0;i<n;i++){
             ans[i]=nums[i];
-        }
-        for(int i=0;i<n;i++){
-            ans[i+n]=nums[i];
+             ans[i+n]=nums[i];
         }
         return ans;    
     }

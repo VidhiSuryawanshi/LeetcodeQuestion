06fd72bee3af84a5f4622beca762f30ef26f7b8e
VidhiSuryawanshi <suryawanshividhi6@gmail.com>
2025-07-11 21:55:42 +0530

    Time: 4 ms (88.85%), Space: 58.5 MB (31.48%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -10,7 +10,7 @@ class Solution {
         slow = nums[0];
         while(slow!=fast){
             slow=nums[slow];
-            fast=nums[nums[fast]];
+            fast=nums[fast];
         }
         return slow;
     }

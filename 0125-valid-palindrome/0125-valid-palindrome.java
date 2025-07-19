0abdd3647856cc1cb69f34a126ea8cc92f191609
VidhiSuryawanshi <suryawanshividhi6@gmail.com>
2025-07-19 13:36:27 +0530

    Time: 2 ms (98.46%), Space: 42.8 MB (95.53%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,63 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s==null || s.length()==0){
+            return false;
+        }
+        int left=0;
+        int right = s.length()-1;
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }      
+        return true;  
+    }
+}
+
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         if(s==null || s.length()==0){
+//             return false;
+//         }
+//         int left=0;
+//         int right=s.length()-1;
+//         while(left<right){
+//             while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+//                 left++;
+//             }
+//             while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+//                 right--;
+//             }
+
+//             if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right)){
+//                 return false;
+//             }
+//             left++;
+//             right--;
+//         }
+//         return true;
+//     }
+// }
+
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         if(s==null || s.length()==0){
+//             return false;
+//         }
+//         s=s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+//         for(int i=0;i<s.length()/2;i++){
+//             if(s.charAt(i)!=s.charAt(s.length()-i-1)){
+//                 return false;
+//             }
+//         }a
+//         return true;
+//     }
+// }

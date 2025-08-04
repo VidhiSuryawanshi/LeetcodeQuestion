6174644999dc12cf1a027bafea1069874f2cebdc
VidhiSuryawanshi <suryawanshividhi6@gmail.com>
2025-08-04 08:26:08 +0530

    Time: 0 ms (100%), Space: 41.5 MB (91.54%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,24 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] str = s.trim().split("\\s+");
-        String a = str[str.length-1];
-        return a.length();
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            char ch = s.charAt(i);
+            if(ch!=' '){
+                count++;
+            }
+            else if(count!=0){
+                break;
+            }
+        }
+        return count;
     }
 }
+
+
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         String[] str = s.trim().split("\\s+");
+//         String a = str[str.length-1];
+//         return a.length();
+//     }
+// }
